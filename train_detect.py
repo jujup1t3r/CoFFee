2e5bd06c02df14c66e621e3d178b0cf0d55a456f
jujup1t3r <apichaya.fow@example.com>
2026-09-10 15:35:41 +0700

    1

--- train_detect.py
+++ train_detect.py
@@ -1,16 +1,21 @@
 from ultralytics import YOLO
 
-# โหลดโมเดลตั้งต้นสำหรับ Object Detection
-model = YOLO("yolov8n.pt")
+def main():
+    # โหลดโมเดลตั้งต้นสำหรับ Object Detection
+    model = YOLO("yolov8n.pt")
 
-# เทรนโมเดลหาตำแหน่งเมล็ดกาแฟ
-model.train(
-    data="Coffee Bean.v5/data.yaml",
-    epochs=15,  
-    imgsz=320,  
-    batch=16,
-    single_cls=True,
-    cache=True,  
-    project="runs/detect",
-    name="bean_detector",
-)
+    # เทรนโมเดลหาตำแหน่งเมล็ดกาแฟ
+    model.train(
+        data="Coffee Bean.v5/data.yaml",
+        epochs=15,
+        imgsz=320,
+        batch=16,
+        single_cls=True,
+        cache=True,
+        project="runs/detect",
+        name="bean_detector",
+        device=0  # บังคับใช้ GPU ตัวที่ 0 (RTX 3050)
+    )
+
+if __name__ == '__main__':
+    main()
